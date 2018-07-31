95a54cbf67d015e8158110d74eb15d1c3ff6d8d5
xinpingdeng <xinping.deng@gmail.com>
2018-07-31 08:53:34 +0000

    should be ready to determine desination automatically for all beams

--- pipeline/capture.py
+++ pipeline/capture.py
@@ -20,35 +20,22 @@ def ConfigSectionMap(conf_fname, section):
             dict_conf[option] = None
     return dict_conf
 
-def main(args):
-    args       = parser.parse_args()
-    conf_fname = args.conf_fname[0]
-    beam       = args.beam[0]
-
-    # Configure metadata interface
-    MCAST_GRP  = ConfigSectionMap(conf_fname, "MetadataInterfaceTOS")['ip']
-    MCAST_PORT = int(ConfigSectionMap(conf_fname, "MetadataInterfaceTOS")['port'])
-    MCAST_ADDR = ('', MCAST_PORT)
+def available_beam(multicast_data, nbeam):
+    routing_table = multicast_data['routing_table']
+    beams = []
+    for beam in range(nbeam):
+        ip = []
+        for table_line in routing_table:
+            chk_ip   = table_line.split(',')[3 * beam + 2]
+            if(chk_ip != "0.0.0.0"):
+                ip.append(chk_ip)
+        if(len(ip)!=0):
+            beams.append(beam)            
+    return np.array(beams)  
     
-    # Create the socket and get ready to receive data
-    sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create the socket
-    group = socket.inet_aton(MCAST_GRP)
-    mreq  = struct.pack('4sL', group, socket.INADDR_ANY)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(MCAST_ADDR)                                    # Bind to the server address
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)  # Tell the operating system to add the socket to the multicast group on all interfaces.
-    
-    # Get data packet
-    pkt, addr = sock.recvfrom(1<<16)
-    data      = json.loads(pkt)
-
-    # Get information from metadata packet
-    target_name = data['target_name']
-    ra  = float(data['beams_direction']['beam{:02d}'.format(beam + 1)][0]) # RA in decimal radian, we may need to convert it to other unit
-    dec = float(data['beams_direction']['beam{:02d}'.format(beam + 1)][1]) # DEC in decimal radian, we may need to convert it to other unit
-
+def destination(multicast_data, conf_fname, beam):    
     # check routing table
-    routing_table = data['routing_table']
+    routing_table = multicast_data['routing_table']
     chk           = 0
     address       = []
     ip_chk        = []
@@ -56,10 +43,11 @@ def main(args):
     for table_line in routing_table:
         chk_ip   = table_line.split(',')[3 * beam + 2]
         chk_port = table_line.split(',')[3 * beam + 3]
+        if(chk_ip != "0.0.0.0" and chk_port != "0"):
+            address.append("{:s}:{:s}".format(chk_ip, chk_port))
+            ip_chk.append("{:s}:{:d}".format(chk_ip, chk))
+            ip.append(chk_ip)
         
-        address.append("{:s}:{:s}".format(chk_ip, chk_port))
-        ip_chk.append("{:s}:{:d}".format(chk_ip, chk))
-        ip.append(chk_ip)        
         chk = chk + 1
 
     # Get software input information from the routing table information
@@ -97,7 +85,7 @@ def main(args):
         print "\t{:s}".format(address_nchk[i])
     
     # Get the number of channel on each IP
-    freq0     = float(data['sky_frequency'])
+    freq0     = float(multicast_data['sky_frequency'])
     nchan_chk = int(ConfigSectionMap(conf_fname, "EthernetInterfaceBMF")['nchan_chk'])
     nchan     = np.zeros(nip, dtype=int)
     freq      = np.zeros(nip, dtype=float)
@@ -105,13 +93,48 @@ def main(args):
         nchan[i] = (max_min_chk[i][1] - max_min_chk[i][0] + 1) * nchan_chk;
         freq[i]  = freq0 - 0.5 * (nchk_beam - (max_min_chk[i][1] + max_min_chk[i][0] + 1)) * nchan_chk 
         print "The center frequency of data from {:s} is {:.1f} MHz with {:d} channels, the login detail is \"{:s}\".".format(ip[i], freq[i], nchan[i], node[i])
+    print "\n"
+    return node, address_nchk
+    
+def main(args):
+    args       = parser.parse_args()
+    conf_fname = args.conf_fname[0]
+
+    # Configure metadata interface
+    MCAST_GRP  = ConfigSectionMap(conf_fname, "MetadataInterfaceTOS")['ip']
+    MCAST_PORT = int(ConfigSectionMap(conf_fname, "MetadataInterfaceTOS")['port'])
+    MCAST_ADDR = ('', MCAST_PORT)
+    
+    # Create the socket and get ready to receive data
+    sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create the socket
+    group = socket.inet_aton(MCAST_GRP)
+    mreq  = struct.pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(MCAST_ADDR)                                    # Bind to the server address
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)  # Tell the operating system to add the socket to the multicast group on all interfaces.
+    
+    # Get data packet
+    pkt, addr      = sock.recvfrom(1<<16)
+    multicast_data = json.loads(pkt)
+
+    # Get information from metadata packet, which is shared with all beams
+    target_name = multicast_data['target_name']
+    ra  = float(multicast_data['beams_direction']['beam01'][0]) # RA in decimal radian, we may need to convert it to other unit
+    dec = float(multicast_data['beams_direction']['beam01'][1]) # DEC in decimal radian, we may need to convert it to other unit
+
+    # To get available beams 
+    nbeam = 36
+    beams = available_beam(multicast_data, nbeam)
+    print "The available beams are {:s}, counting from 0 ...\n".format(beams)
+    
+    # Get the desination of a given beam
+    for beam in beams:
+        node, address_nchk = destination(multicast_data, conf_fname, beam)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read TOS metadata and get required information')
     parser.add_argument('-a', '--conf_fname', type=str, nargs='+',
                         help='The name of configuration file')
-    parser.add_argument('-b', '--beam', type=int, nargs='+',
-                        help='The beam index, 0-35')
-    
+
     args = parser.parse_args()
     main(args)
