681febec15a386b5320c51b2fcb7184015c55fc8
xinpingdeng <xinping.deng@gmail.com>
2019-05-22 15:02:36 +0200

    backup

--- script/heimdall_waterfaller.py
+++ script/heimdall_waterfaller.py
@@ -6,13 +6,13 @@ import matplotlib.colors
 import os
 
 source = "J1819-1458"
-source = "J0332+5434"
-source = "J1939+2134"
+#source = "J0332+5434"
+#source = "J1939+2134"
 #source = "J1713+0747"
 
-#nchunk = 30
+nchunk = 30
 #nchunk = 48
-nchunk = 24
+#nchunk = 24
 nchan  = 512
 tsamp  = 216
 
@@ -24,7 +24,7 @@ fil_fname  = os.path.join(ddir, fil_fname)
 cand_fname = os.path.join(ddir, cand_fname)
 cand       = np.loadtxt(cand_fname)
 ncand      = len(cand)
-shift      = 0.3
+shift      = 0.05
 
 for i in range(ncand):
     tstart   = cand[i,2] - shift
