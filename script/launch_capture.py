26504f5e46dd81ce938c75e0b508837ef614c01e
root <root@edd01.mpifr-bonn.mpg.de>
2020-11-27 14:29:39 +0000

    -

---
+++ script/launch_capture.py
@@ -0,0 +1,40 @@
+import subprocess
+import time
+import os
+import pty
+import argparse
+from argparse import RawTextHelpFormatter
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Launch container for PAF pipeline development')
+    parser.add_argument('-i', '--numa_id', action="store", dest="nid", help='The ID of NUMA node')
+
+    disk="/beegfsEDD/NESSER"
+    dockerimage="edd01:5000/capture_bypassed_bmf"
+    dockername="numa_"+parser.parse_args().nid
+
+    cmd = "docker run --name="+dockername+" --rm \
+        --privileged=true \
+        --ipc=shareable \
+        --cap-add=IPC_LOCK \
+        --ulimit memlock=-1:-1 \
+        --net=host \
+        -v "+disk+":"+disk+" \
+        -e DISPLAY \
+        -e USER=root \
+        --runtime=nvidia \
+        -e NVIDIA_VISIBLE_DEVICES=0 \
+        -e NVIDIA_DRIVER_CAPABILITIES=all \
+        --cap-add=SYS_PTRACE \
+        -it "+dockerimage+" /bin/bash -ic 'cd phased-array-feed/;git pull;python script/start_bypassed_capture.py -i "+parser.parse_args().nid+"'"
+
+
+    pty, tty = pty.openpty()
+    print(cmd)
+
+    p1 = subprocess.Popen(cmd, shell=True)#,stdin=tty, stdout=tty, stderr=tty)
+
+    raw_input("Press key to stop...")
+
+    os.system("docker container stop "+dockername)
+    print("container stopped")
--- script/start_bypassed_capture.py
+++ script/start_bypassed_capture.py
@@ -92,25 +92,25 @@ def synced_refinfo(utc_start_capture, ip, port):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='options', formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--key', '-k', action = "store", default ="dada", dest = "key", help = "Shared memory key")
+    # parser.add_argument('--key', '-k', action = "store", default ="dada", dest = "key", help = "Shared memory key")
     parser.add_argument('--node_id', '-nid', action = "store", default =0, dest = "nodeid", help = "ID of the numa node")
     # parser.add_argument('--time_ref', '-ref', action = "store", dest = "time_ref", help = "Time reference string epoch_seconds_dataframe")
-    parser.add_argument('--ip_addr', '-ip', action = "store", dest = "ip_addr", help = "Ip address")
+    # parser.add_argument('--ip_addr', '-ip', action = "store", dest = "ip_addr", help = "Ip address")
     # parser.add_argument('--freq', '-f', action = "store", dest = "freq", help = "Base frequency of channel group")
     # parser.add_argument('--capture_conf', '-c', action = "store", dest = "capture_conf", help = "Capture conf of single capture thread: ip_port_exepectedbeams_actualbeams_cpuid")
-    parser.add_argument('--packets', '-p', action = "store", dest = "packets", help = "Number of packets stored in one ringerbuffer block. Note: packet*nof_beam*packets_size = ringubffer_size")
-    parser.add_argument('--temp_packets', '-tp', action = "store", dest = "temp_packets", default=128, help = "Number of packets stored temporary buffer. Note: temp_packet*nof_beam*packets_size = tempbuffer_size")
+    # parser.add_argument('--packets', '-p', action = "store", dest = "packets", help = "Number of packets stored in one ringerbuffer block. Note: packet*nof_beam*packets_size = ringubffer_size")
+    # parser.add_argument('--temp_packets', '-tp', action = "store", dest = "temp_packets", default=128, help = "Number of packets stored temporary buffer. Note: temp_packet*nof_beam*packets_size = tempbuffer_size")
 
-    key = parser.parse_args().key
+    # key = parser.parse_args().key
     nodeid = parser.parse_args().nodeid
     # time_ref = parser.parse_args().time_ref
     # freq = parser.parse_args().freq
     # capture_conf = parser.parse_args().capture_conf
-    packets = int(parser.parse_args().packets)
-    temp_packets = parser.parse_args().temp_packets
-    ip_addr = parser.parse_args().ip_addr
+    # packets = int(parser.parse_args().packets)
+    # temp_packets = parser.parse_args().temp_packets
+    # ip_addr = parser.parse_args().ip_addr
 
-    print(key, nodeid, packets, temp_packets, ip_addr)
+    print(nodeid)
     # block_size = packets*36*PAF_DF_PACKETSZ
     # nof_blocks = 8
     # disk_folder = "/beegfsEDD/NESSER"
